7786277d37f28086e3ff02fd4db254af308d2ca8
fabiankd <fabiankd31@gmail.com>
2022-04-22 09:12:11 +0200

    fitting

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import time
+import warnings
 import xml.etree.ElementTree as ET
+
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import warnings
 from sklearn.metrics import r2_score
-import time
+from scipy.optimize import curve_fit, optimize
+from lmfit import minimize, Parameters, Parameter, report_fit
+import sympy
 
 tree = ET.parse('/Users/fabiankading/PycharmProjects/pr2/HY202103_D08_02_LION1_DCM_LMZC.xml')
 root = tree.getroot()
@@ -28,16 +31,26 @@ print(current)
 voltage_x = np.array(voltage)
 current_y = np.array(current)
 
-current_y = [i**2 for i in current_y]
-current_y = [i*10e9 for i in current_y]
-#current_y = [abs(i * 10e9) for i in current_y]
+current_y = [abs(i) for i in current_y]
+print(current_y)
+voltagea = voltage_x[0:10]
+currenta = current_y[0:10]
+p2a = np.poly1d(np.polyfit(voltagea, currenta, 8))
 
-p2 = np.poly1d(np.polyfit(voltage_x, current_y, 2))
-print(p2)
+x_werte = voltage_x[0:8]
 
+y = 2 * 10 ** -10 * x_werte ** 2 + 10 ** -10 * x_werte + 4 * 10 ** -11
+
+plt.plot(x_werte, y)
+
+voltageb = voltage_x[8:]
+currentb = current_y[8:]
+p2b = np.poly1d(np.polyfit(voltageb, currentb, 7))
 
 plt.plot(voltage_x, current_y, color='black', marker='o')
-plt.plot(voltage_x, p2(current_y), color='red')
+
+plt.plot(voltagea, p2a(currenta), color='red')
+plt.plot(voltageb, p2b(currentb), color='blue')
 plt.yscale('log')
 plt.show()
 
@@ -53,44 +66,44 @@ plt.grid('true')
 plt.tight_layout()
 plt.show()
 
-wavelenght = []
+wavelength = []
 for child in root.findall('./ElectroOpticalMeasurements/ModulatorSite/Modulator/PortCombo/WavelengthSweep'):
-    wavelenght.append(child.attrib)
+    wavelength.append(child.attrib)
     for i in child:
-        wavelenght.append(list(map(float, i.text.split(','))))
+        wavelength.append(list(map(float, i.text.split(','))))
 
 result = []
 x = [1, 4, 7, 10, 13, 16, 19]
 for i in x:
-    result.append(np.array(wavelenght[i + 1]) - np.array(wavelenght[20]))
+    result.append(np.array(wavelength[i + 1][0:6065]) - np.array(wavelength[20][0:6065]))
 
 for i in x:
-    print(len(wavelenght[i]))
+    print(len(wavelength[i]))
 
 print(len(result), 'hier')
 
-print(wavelenght[0]['DCBias'])
-print(len(wavelenght))
-plt.plot(wavelenght[1], result[0], label=wavelenght[0]['DCBias'])
-plt.plot(wavelenght[4], result[1], label=wavelenght[3]['DCBias'])
-plt.plot(wavelenght[7], result[2], label=wavelenght[6]['DCBias'])
-plt.plot(wavelenght[10], result[3], label=wavelenght[9]['DCBias'])
-plt.plot(wavelenght[13], result[4], label=wavelenght[12]['DCBias'])
-plt.plot(wavelenght[16], result[5], label=wavelenght[15]['DCBias'])
-plt.plot(wavelenght[19], wavelenght[20], label=wavelenght[18]['DCBias'], color='black')
+print(wavelength[0]['DCBias'])
+print(len(wavelength))
+plt.plot(wavelength[1], result[0], label=wavelength[0]['DCBias'])
+plt.plot(wavelength[4], result[1], label=wavelength[3]['DCBias'])
+plt.plot(wavelength[7], result[2], label=wavelength[6]['DCBias'])
+plt.plot(wavelength[10], result[3], label=wavelength[9]['DCBias'])
+plt.plot(wavelength[13], result[4], label=wavelength[12]['DCBias'])
+plt.plot(wavelength[16], result[5], label=wavelength[15]['DCBias'])
+plt.plot(wavelength[19], wavelength[20], label=wavelength[18]['DCBias'], color='black')
 plt.legend(fontsize='small', title='DCBias in V', ncol=2)
 plt.xlabel('Wavelenght in nm')
 plt.ylabel('Measured transmission in dB')
 plt.title('Transmission spectral')
 plt.show()
 
-plt.plot(wavelenght[1], wavelenght[2], label=wavelenght[0]['DCBias'])
-plt.plot(wavelenght[4], wavelenght[5], label=wavelenght[3]['DCBias'])
-plt.plot(wavelenght[7], wavelenght[8], label=wavelenght[6]['DCBias'])
-plt.plot(wavelenght[10], wavelenght[11], label=wavelenght[9]['DCBias'])
-plt.plot(wavelenght[13], wavelenght[14], label=wavelenght[12]['DCBias'])
-plt.plot(wavelenght[16], wavelenght[17], label=wavelenght[15]['DCBias'])
-plt.plot(wavelenght[19], wavelenght[20], label=wavelenght[18]['DCBias'], color='black')
+plt.plot(wavelength[1], wavelength[2], label=wavelength[0]['DCBias'])
+plt.plot(wavelength[4], wavelength[5], label=wavelength[3]['DCBias'])
+plt.plot(wavelength[7], wavelength[8], label=wavelength[6]['DCBias'])
+plt.plot(wavelength[10], wavelength[11], label=wavelength[9]['DCBias'])
+plt.plot(wavelength[13], wavelength[14], label=wavelength[12]['DCBias'])
+plt.plot(wavelength[16], wavelength[17], label=wavelength[15]['DCBias'])
+plt.plot(wavelength[19], wavelength[20], label=wavelength[18]['DCBias'], color='black')
 
 plt.legend(fontsize='small', title='DCBias in V', ncol=2)
 plt.xlabel('Wavelenght in nm')
@@ -98,8 +111,8 @@ plt.ylabel('Measured transmission in dB')
 plt.title('Transmission spectral')
 plt.show()
 
-x = np.array(wavelenght[19])
-y = np.array(wavelenght[20])
+x = np.array(wavelength[19])
+y = np.array(wavelength[20])
 
 z = np.polyfit(x, y, 4)
 p = np.poly1d(z)
@@ -122,6 +135,10 @@ plt.plot(x, y, linewidth=0.5)
 plt.plot(x, p(x), 'r--')
 plt.plot(x, p2(x), 'g')
 plt.plot(x, p30(x), 'b')
+n_max = y.argmax()
+plt.plot(x[n_max], y[n_max], 'o')
+n_min = y.argmin()
+plt.plot(x[n_min], y[n_min], 'o')
 plt.show()
 
 r2_2 = r2_score(y, p2(x))
@@ -132,8 +149,53 @@ print('2th degree:', r2_2 * 100)
 print('4th degree:', r2_4 * 100)
 print('30th degree:', r2_30 * 100)
 
-x = list(range(0, len(wavelenght), 3))
-for i in x:
-    plt.plot(wavelenght[i + 1], wavelenght[i + 2], label=wavelenght[i]['DCBias'])
+liste = list(range(0, len(wavelength), 3))
+for i in liste:
+    plt.plot(wavelength[i + 1], wavelength[i + 2], label=wavelength[i]['DCBias'])
 
+plt.show()
+
+xdata = np.array(wavelength[19])
+ydata = np.array(wavelength[20])
+liste_p2 = np.poly1d(np.polyfit(xdata, ydata, 2))
+plt.plot(x, y, linewidth=0.5)
+
+p2 = liste_p2(ydata)
+n_max = p2.argmax()
+plt.plot(xdata[n_max], liste_p2[n_max], 'o')
+n_min = p2.argmin()
+plt.plot(xdata[n_min], liste_p2[n_min], 'o')
+
+plt.show()
+
+# plot iv measurement
+# load varible
+x_daten = np.array(voltage)
+y_daten = np.array(current)
+y_daten = [abs(i) for i in current_y]
+
+x_werte_1 = x_daten[0:8]
+
+y_werte_1 = 2 * 10 ** -10 * x_werte ** 2 + 10 ** -10 * x_werte + 4 * 10 ** -11
+
+plt.plot(x_daten, y_daten, 'x',  label='Punkte')
+plt.plot(x_werte_1, y_werte_1)
+
+def func(x, a, b, c):
+    return a * b**x + c
+
+
+x_werte_2 = x_daten[8:12]
+y_werte_2 = y_daten[8:12]
+
+
+
+popt, pcov = curve_fit(func, x_werte_2, y_werte_2)
+print(popt)
+
+xFit = np.arange(0, 1, 0.001)
+
+plt.plot(xFit, func(xFit, *popt), 'r', label='test')
+plt.legend()
+plt.yscale('log')
 plt.show()
